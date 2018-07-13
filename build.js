15f6fa9b5ee984ad9c5d1bca89a256c582c6dd94
Alexander Elias <alex.steven.elias@gmail.com>
2018-07-13 16:49:26 -0700

    added new build, added dash styles

--- build.js
+++ build.js
@@ -2,39 +2,64 @@
 
 const Fs = require('fs');
 const Util = require('util');
+const Path = require('path');
 const Postcss = require('postcss');
 const Cssnano = require('cssnano');
 const Package = require('./package');
 const Cssnext = require('postcss-cssnext');
 
+const ReadFolder = Util.promisify(Fs.readdir);
 const ReadFile = Util.promisify(Fs.readFile);
 const WriteFile = Util.promisify(Fs.writeFile);
 
 const header = `/*
-	Name: Boxers
+	Name: ${Package.name}
 	Version: ${Package.version}
-	License: MPL-2.0
-	Author: Alexander Elias
-	Email: alex.steven.elias@gmail.com
+	License: ${Package.license}
+	Author: ${Package.author}
+	Email: ${Package.email}
 	This Source Code Form is subject to the terms of the Mozilla Public
 	License, v. 2.0. If a copy of the MPL was not distributed with this
 	file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 `;
 
+const build = async function (name, next) {
+	let result;
+
+	const data = await ReadFile('src/' + name + '.css');
+
+	// regular
+	if (next) {
+		result = await Postcss([Cssnext]).process(data, {
+			from: 'src/' + name + '.css',
+			to: 'dis/' + name + '.css'
+		});
+	} else {
+		result = {};
+		result.css = data;
+	}
+
+	WriteFile('dis/' + name + '.css', `${header}${result.css}`);
+
+	// minified
+	result = await Postcss([Cssnano]).process(result.css, {
+		from: 'src/' + name  + '.css',
+		to: 'dis/'  + name + '.min.css'
+	});
+
+	WriteFile('dis/' + name + '.min.css', `${header}${result.css}`);
+
+};
+
 (async function() {
 
-	let result;
-	const options = { from: 'src/boxers.css', to: 'dis/boxers.css' };
-	const data = await ReadFile('src/index.css');
+	const items = await ReadFolder('src');
 
-	result = await Postcss([Cssnext]).process(data, options);
-
-	WriteFile('dis/boxers.css', `${header}${result.css}`);
-
-	result = await Postcss([Cssnano]).process(result.css, options);
-
-	WriteFile('dis/boxers.min.css', `${header}${result.css}`);
+	for (let item of items) {
+		let name = Path.basename(item, '.css');
+		await build(name, name === 'boxers');
+	}
 
 }()).catch(function (error) {
 	console.error(error);
